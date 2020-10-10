f07246e58a75e6049aedfd8d3356ff91092397d3
AshnaSathesh <60621991+AshnaSathesh@users.noreply.github.com>
2020-10-10 19:07:16 +0530

    First Commit
    Adding the resources

---
+++ time_app/lib/pages/loading.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:time_app/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime': instance.isDayTime,
+    });
+  }
+
+
+   @override
+   void initState() {
+     super.initState();
+     setupWorldTime();
+   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red[800],
+      body: Center(
+        child: SpinKitDualRing(
+          color: Colors.white,
+          size: 50.0,
+        ),
+      ),
+    );
+  }
+}
